5ab54fe24dcc05127122f1fbd91ad52efe813b91
Shaahin <rezanajafi564@gmail.com>
2022-06-28 17:11:47 +0430

    dynamicButtons-and-static-hambuergerMenu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,24 +1,52 @@
 // swipers
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    loop: true,
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    570: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    breakpoints: {
-      570: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      992: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 40,
     },
-  });
+  },
+});
+
+$("#filterResultNovelties").hide();
+$("#filterCheckboxNovelties").click(function () {
+  if ($(this).is(":checked")) {
+    $("#filterResultNovelties").show(100);
+  } else {
+    $("#filterResultNovelties").hide(100);
+  }
+});
+
+$("#filterResultDiscounted").hide();
+$("#filterCheckboxDiscounted").click(function () {
+  if ($(this).is(":checked")) {
+    $("#filterResultDiscounted").show(100);
+  } else {
+    $("#filterResultDiscounted").hide(100);
+  }
+});
+
+function handleSwitcherNovelties() {
+  $('#filterCheckboxNovelties').attr('checked', false);
+  $("#filterResultNovelties").hide(100);
+};
+
+function handleSwitcherDiscounted() {
+  $('#filterCheckboxDiscounted').attr('checked', false);
+  $("#filterResultDiscounted").hide(100);
+}
 
 
 //   var rangeOne = document.querySelector('input[name="rangeOne"]'),
